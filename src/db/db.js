99d003306f17839547c698eab244dcd87f038d36
FranHutinel <m.ohutinel@gmail.com>
2023-03-23 16:43:47 -0300

    se agrega enlace a github

--- src/db/db.js
+++ src/db/db.js
@@ -2,11 +2,11 @@ import pkg from 'pg';
 const {Pool} = pkg;
 
 const pool = new Pool({
-    host: '',
-    database: '',
+    host: 'aassaas',
+    database: 'nombrebd',
     port: 5432,
-    user: '',
-    password: '',
+    user: 'usuario',
+    password: 'contrasena1',
     max: 5,
     idleTimeoutMillis: 10000,
     connectionTimeoutMillis: 2000,
